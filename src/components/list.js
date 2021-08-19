df8ab3a3bb5059053c7ea09b967c4b6631603e3b
Sokiyna <sokiynanaser@yahoo.com>
2021-08-19 16:45:08 +0300

    lab31

---
+++ src/components/list.js
@@ -0,0 +1,83 @@
+import React, { useContext, useEffect, useState } from 'react';
+import { ListContext } from '../../context/listContext';
+import { SettingContext } from '../../context/settingsContext';
+function TodoList(props) {
+  const listContext = useContext(ListContext);
+  const settingsContext = useContext(SettingContext);
+  const [page, setpage] = useState(0);
+  const [list, setList] = useState([]);
+  useEffect(() => {
+    if (settingsContext.view) {
+      setList(listContext.list)
+    } else {
+      let temp = [];
+      temp = listContext.list.filter((item) => {
+        if (!item.complete) {
+          return (item);
+        }
+      })
+      setList(temp);
+    }
+  }, [])
+  useEffect(() => {
+    if (settingsContext.view) {
+      setList(listContext.list)
+    } else {
+      let temp = [];
+      temp = listContext.list.filter((item) => {
+        if (!item.complete) {
+          return (item);
+        }
+      })
+      setList(temp);
+    }
+  }, [settingsContext.view])
+  function nextPage() {
+    settingsContext.nextpage();
+    setpage(page + 1);
+  }
+  function prePage() {
+    settingsContext.previouspage();
+    setpage(page - 1);
+  }
+  function modifyList(id) {
+    listContext.toggleComplete(id);
+    if (settingsContext.view) {
+      setList(listContext.list)
+    } else {
+      let temp = [];
+      temp = listContext.list.filter((item) => {
+        if (!item.complete) {
+          return (item);
+        }
+      })
+      setList(temp);
+    }
+  }
+  return (
+    <>
+      <ul>
+        {list.map((item, idx) => {
+          if (idx >= settingsContext.start && idx <= settingsContext.end) {
+            return (
+              <li
+                className={`complete-${item.complete.toString()}`}
+                key={item._id}
+              >
+                <span onClick={() => modifyList(item._id)}>
+                  {item.text}
+                </span>
+              </li>
+            )
+          }
+        })}
+      </ul>
+      {page > 0 && <button onClick={prePage}>previous</button>}
+      {!(page == (Math.ceil(listContext.list.length / settingsContext.numberOfItems) - 1)) && <button onClick={nextPage}>Next</button>}
+      <button onClick={() => { settingsContext.setSettings(null, !settingsContext.view) }}>done</button>
+    </>
+  );
+}
+
+
+export default TodoList;
